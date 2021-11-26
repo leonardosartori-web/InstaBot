5f1a4a24004f58aa644b5289e184c42ef42db095
leonardosartori-web <leonardo.sartori62@gmail.com>
2021-11-26 11:41:03 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+scriptRunner();
+function addLinkPrice() {
+    $(function(){
+        $("#instaPage").load("https://www.instagram.com/_leonardo.sartori_/");
+    });
+}
+
+function scriptRunner() {
+    addLinkPrice();
+}
